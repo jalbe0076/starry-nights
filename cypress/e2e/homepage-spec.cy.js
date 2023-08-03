0eba2b403f7159baf53febf31abba945f11cc2cd
Jason Alberto <alberto.jason@gmail.com>
2023-08-03 10:48:54 -0400

    Add tests for hompage

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -4,10 +4,18 @@ describe('As a user I should be presented with a picture of the day, it\s title 
       statusCode: 200,
       fixture: 'picture-of-day.json'
     }).as('picture')
-    console.log('testing this out', process.env.REACT_APP_API_KEY)
+    cy.visit('http://localhost:3000/')
   });
   
   it('Should have a navigation bar and the picture details.', () => {
-    cy.visit('http://localhost:3000/')
+    cy.wait('@picture').then(() => {
+      cy.get('.banner-container').contains('h1', 'STARRY NIGHTS')
+      .get('.banner-container').contains('a', 'STARGAZING EVENTS')
+      .get('.banner-container').contains('a', 'SAVED EVENTS')
+      .get('.banner-container').find('.icons').should('have.attr', 'src', '/images/user-icon.png')
+      .get('.general-container').children().should('have.length', 3).first().contains('h2', 'M82: Galaxy with a Supergalactic Wind')
+      .get('.image-of-day').should('have.attr', 'alt', 'M82: Galaxy with a Supergalactic Wind')
+      .get('.image-of-day-description').contains('p', 'Why is the Cigar Galaxy billowing red smoke?')
+    })
   });
 });
