570fd0ffcd8099c655fa86e333faf18765c0e4d5
Jason Alberto <alberto.jason@gmail.com>
2023-08-03 14:33:35 -0400

    Add delete button and responsive styling on saved event component

--- src/components/SavedEvents/SavedEvents.js
+++ src/components/SavedEvents/SavedEvents.js
@@ -10,7 +10,10 @@ const SavedEvents = ({ savedEvents, handleEventList }) => {
   const savedEventList = savedEvents.map((event) => {
     const id = nanoid();
     return (
+      <div className='delete-container'>
+      <button className='delete-event-btn'><img className='delete-icon' src={process.env.PUBLIC_URL + '/images/close-icon.png'} /> </button>
       <EventList handleEventList={handleEventList} data={event} key={id} id={id} />
+      </div>
     );
   });
 
@@ -18,11 +21,12 @@ const SavedEvents = ({ savedEvents, handleEventList }) => {
     <section className='general-container'>
       <h2 className='events-subtitle'>Celestial Appointments</h2>
       <p className='saved-subtitle' >Get ready for upcoming celestial close approaches, click on any event to get more information. </p>
-      <div className='upcoming-container'>
-        <p className='upcoming-item list-leader'>Date & Time</p>  
-        <p className='upcoming-item list-leader'>Designation</p>
+      <div className='saved-upcoming'>
+        <p className='list-leader'>Date,Time & Designation</p>  
       </div>
+      <div className='saved-events-container'>
       {savedEventList}
+      </div>
     </section>
   )
 };
